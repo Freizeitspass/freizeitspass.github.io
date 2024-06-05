0cf199763211a86775c826e91e1e8bf656bd6b86
laurauniibk <l.eddelbuettel@hotmail.com>
2024-06-05 20:35:26 +0200

    main.js erstellt und Daten Links

---
+++ main.js
@@ -0,0 +1,6 @@
+//  Data GV AT : https://www.data.gv.at/suche/?typeFilter%5B%5D=dataset&searchterm=tirol&searchin=data&formatFilter%5B%5D=JSON Quellen
+// Weitwanderwege: https://www.tirol.at/reisefuehrer/sport/wandern/weitwandern
+// Weitwanderwege andere Quelle: https://www.almenrausch.at/touren/mehrtagestouren-sommer/tirol/
+// mögliche Quelle für Almen/Gasthäuser: https://www.almenrausch.at/einkehr-uebernachtung/gasthoefe-alpengasthoefe-gasthaeuser/tirol/
+// weitere Quelle: https://geohub-1-magibk.hub.arcgis.com/apps/f405522fae6e4e38b1feb0029c7f4817/explore
+// über Data DV AT gefunden: https://data-tiris.opendata.arcgis.com/datasets/almzentren-1 
