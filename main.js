9e38d31136ab41ff2bbb079b488263f326344ecb
Ylenia <05ylenia@gmail.com>
2024-06-10 10:56:41 +0200

    Maßstab und Popup zur Karte hinzugefügt

--- main.js
+++ main.js
@@ -6,9 +6,27 @@
 // über Data DV AT gefunden: https://data-tiris.opendata.arcgis.com/datasets/almzentren-1 
 
 //Map initialisieren
-var map = L.map('map').setView([47.268333, 11.393333], 11);
+let lat = 47.268333;
+let lng = 11.393333;
+let zoom = 11; 
+
+let map = L.map('map', {
+    center: [lat, lng],
+    zoom: zoom
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//Maßstab hinzugefügt
+L.control.scale({
+    imperial: false,
+}).addTo(map); 
+
+//Popup hinzugefügt
+let marker = L.marker ([47.268333, 11.393333]).addTo(map); 
+marker.bindPopup (`
+<b> Innsbruck </b>
+`).openPopup(); 
