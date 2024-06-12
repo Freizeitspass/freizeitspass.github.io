dee364f452927ab9d7817552b2d16481ab4b53a9
laurauniibk <l.eddelbuettel@hotmail.com>
2024-06-12 14:18:02 +0200

    seite ueber erstellt

---
+++ ueber/main.js
@@ -0,0 +1,45 @@
+
+//Map initialisieren
+let lat = 47.268333;
+let lng = 11.393333;
+let zoom = 9;
+
+let map = L.map("map", {
+    fullscreenControl: true,
+}).setView([lat, lng], 11);
+
+// thematische Layer
+let themaLayer = {
+    route: L.featureGroup(),
+}
+
+// WMTS Hintergrundlayer der eGrundkarte Tirol
+let eGrundkarteTirol = {
+    sommer: L.tileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+    }),
+    nomenklatur: L.tileLayer("https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+        attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
+        pane: "overlayPane",
+    }),
+}
+
+// Hintergrundlayer eGrundkarte Tirol
+L.control.layers({
+    "eGrundkarte Tirol Sommer": L.layerGroup([
+        eGrundkarteTirol.sommer,
+        eGrundkarteTirol.nomenklatur
+    ]).addTo(map),
+}).addTo(map);
+
+//Maßstab 
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// MiniMap 
+new L.Control.MiniMap(L.tileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+    attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
+}), {
+    toggleDisplay: true,
+}).addTo(map);
