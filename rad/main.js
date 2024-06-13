70937d5c97c5c37ae5863b42f3b2f0e5cc6ace34
laurauniibk <l.eddelbuettel@hotmail.com>
2024-06-13 13:07:44 +0200

    Dropdown Menü integriert in main.css und in rad html und main.js

--- rad/main.js
+++ rad/main.js
@@ -1,1 +1,12 @@
 //Mal schauen was hier dann noch reinmuss; Karten jetzt alle im eigenen JS
+
+//DropDown Menü für Navigation 
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.dropdown-content a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
