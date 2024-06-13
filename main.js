a3214bd64949cd0f7c34b188b1570891e3ad47ed
Ylenia <05ylenia@gmail.com>
2024-06-13 16:22:46 +0200

    Slideshow im JS hinzugefügt

--- main.js
+++ main.js
@@ -135,14 +135,37 @@ function positionPopup() {
 
     popupContainer.style.top = popupTop + 'px';
     popupContainer.style.left = popupLeft + 'px';
-}
-
-// Position des Popups aktualisieren, wenn sich die Karte ändert
-map.on('resize', positionPopup);
-map.on('move', positionPopup);
+}; 
 
 // Initial Popup positionieren
 positionPopup();
 
+//Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
