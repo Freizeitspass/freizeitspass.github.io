93b436781717cc1d2b1ff306b09c3f3658a0ef41
MeritKoch <mkoch41@smail.uni-koeln.de>
2024-06-19 10:40:14 +0200

    Scroll to Top Button Wandern

--- wandern/main.js
+++ wandern/main.js
@@ -82,3 +82,25 @@ map.addControl(rainviewer);
 
 // Locate Control
 L.control.locate().addTo(map);
+
+// Nach oben scrollen Button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+scrollToTopBtn.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
