945343b800c8b338cec4fb5702b1be092638c880
laurauniibk <l.eddelbuettel@hotmail.com>
2024-06-23 21:41:35 +0200

    popups für Radwerkstatt und Cafe für alle Routen hinzugefügt

--- rad/map4.js
+++ rad/map4.js
@@ -5,11 +5,17 @@ let lng4 = 11.393333;
 document.addEventListener('DOMContentLoaded', function () {
     let map4 = L.map("map4", {
         center: [lat4, lng4],
-        zoom: 4,
+        zoom: 11,
         fullscreenControl: true,
         gestureHandling: true,
         mapTypeId: 'terrain',
     });
+
+    // thematische Layer
+    let themaLayer = {
+        bikeshop: L.featureGroup(),
+        coffee: L.featureGroup()
+    };
 
     // WMTS Hintergrundlayer 
     let eGrundkarteTirol = {
@@ -29,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Hintergrundlayer 
-    L.control.layers({
+    let layerControl = L.control.layers({
         "eGrundkarte Tirol Sommer": L.layerGroup([
             eGrundkarteTirol.sommer,
             eGrundkarteTirol.nomenklatur
@@ -39,6 +45,9 @@ document.addEventListener('DOMContentLoaded', function () {
             eGrundkarteTirol.nomenklatur
         ]),
         "OpenStreetMap": openStreetMap,
+    }, {
+        "Fahrradgeschäfte/-reparatur": themaLayer.bikeshop.addTo(map4),
+        "Cafés/Restaurants": themaLayer.coffee.addTo(map4),
     }).addTo(map4);
 
     //Style Elevation
@@ -52,6 +61,195 @@ document.addEventListener('DOMContentLoaded', function () {
         edgeScale: false,
     }).addTo(map4);
     controlElevation.load("data/seefeld.gpx");
+
+    //Bikeshops Liste
+    const BIKESHOP = [
+        {
+            title: "Alpin Bike - Fahrradservice & Reparatur",
+            link: "http://www.alpin-bike.at/",
+            ort: "Innsbruck",
+            lat: 47.27877864015659,
+            lng: 11.370899941018507,
+
+        },
+        {
+            title: "Bike Klinik Innsbruck",
+            link: "https://www.bike-klinik.com/",
+            ort: "Innsbruck",
+            lat: 47.27482897493371,
+            lng: 11.404881058358859,
+        },
+        {
+            title: "Fahrradladen Innsbruck",
+            link: "https://www.fahrradladen-innsbruck.at/",
+            ort: "Innsbruck",
+            lat: 47.25522225775388,
+            lng: 11.396223510784315,
+        },
+        {
+            title: "Veloflott GmbH",
+            link: "https://www.veloflott.at/",
+            ort: "Innsbruck",
+            lat: 47.276823813800476,
+            lng: 11.412865313018468,
+        },
+        {
+            title: "WAMS Radwerkstatt Conrad",
+            link: "-",
+            ort: "Innsbruck",
+            lat: 47.262814924800566,
+            lng: 11.378780777203447,
+        },
+        {
+            title: "Radsport Neuner GmbH",
+            link: "https://www.bikestoreneuner.com/",
+            ort: "Kematen in Tirol",
+            lat: 47.26420710613028,
+            lng: 11.269072803358116,
+        },
+        {
+            title: "Bikesport Baumgartner",
+            link: "https://www.sport-baumgartner.at/",
+            ort: "Völs",
+            lat: 47.25835240590591,
+            lng: 11.32184908194286,
+        },
+        {
+            title: "Radl Werkstatt - MANG Thomas",
+            link: "-",
+            ort: "Zirl",
+            lat: 47.27074145483338,
+            lng: 11.253307261129905,
+        },
+        {
+            title: "PWcycle - Fahrradreparatur & Service",
+            link: "https://www.pwcycle.at/",
+            ort: "Pettnau",
+            lat: 47.291376802909305,
+            lng: 11.159121193571826,
+        },
+        {
+            title: "Bikepalast Telfs -Cubestore Telfs",
+            link: "-",
+            ort: "Telfs",
+            lat: 47.313282961343546,
+            lng: 11.101218725701125,
+        },
+        {
+            title: "Fahrradverleih & Service",
+            link: "-",
+            ort: "Leutasch",
+            lat: 47.37162804962264,
+            lng: 11.164794159227261,
+        },
+
+    ]
+
+
+    //Bikeshops Popups
+    for (let bikeshop of BIKESHOP) {
+        L.marker([bikeshop.lat, bikeshop.lng], {
+            icon: L.icon({
+                iconUrl: `icons/bicycle_shop.png`,
+                popupAnchor: [0, -37],
+                iconAnchor: [16, 37],
+            })
+        })
+            .addTo(themaLayer.bikeshop)
+            .bindPopup(`<b>${bikeshop.title}</b> <br> ${bikeshop.ort} | <a href="${bikeshop.link}">Zur Website</a> 
+`)
+    };
+
+    //Cafes Liste
+    const COFFEE = [
+        {
+            title: "manni.coffee",
+            link: "-",
+            ort: "Innsbruck",
+            lat: 47.26537316391705,
+            lng: 11.394063993012983,
+
+        },
+        {
+            title: "haepinest",
+            link: "https://haepinest.at/",
+            ort: "Innsbruck",
+            lat: 47.271126885551034,
+            lng: 11.391991823597085,
+        },
+        {
+            title: "coffeekult Kaffeerösterei",
+            link: "http://www.coffeekult.com/",
+            ort: "Innsbruck",
+            lat: 47.25652829874937,
+            lng: 11.395917733227401,
+        },
+        {
+            title: "Wild Bean Café",
+            link: "https://www.bp.com/de_at/austria/home/produkte-und-services/fuer-sie/kaffee-und-genuss.html",
+            ort: "Telfs",
+            lat: 47.30741967264585,
+            lng: 11.078996947403871,
+        },
+        {
+            title: "KAFFEE 13",
+            link: "http://www.kaffee13.at/",
+            ort: "Zirl",
+            lat: 47.27214748264314,
+            lng: 11.242332219624108,
+        },
+        {
+            title: "landhauscafe Schöpf",
+            link: "-",
+            ort: "Mösern",
+            lat: 47.31169409574331,
+            lng: 11.146176303053728,
+        },
+        {
+            title: "Cafe Niveau",
+            link: "-",
+            ort: "Seefeld in Tirol",
+            lat: 47.32732860000119,
+            lng: 11.187831125506147,
+        },
+        {
+            title: "Sport Cafe",
+            link: "-",
+            ort: "Leutasch",
+            lat: 47.37031045346174,
+            lng: 11.162769722882114,
+        },
+        {
+            title: "Café Weisse Katz",
+            link: "-",
+            ort: "Inzing",
+            lat: 47.27394380389983,
+            lng: 11.194932535135209,
+        },
+        {
+            title: "Thomas ́ KAFFEE-& GENUSSMANUFAKTUR",
+            link: "https://www.thomas-kaffee.tirol/",
+            ort: "Telfs",
+            lat: 47.31987815442093,
+            lng: 11.072045838994661,
+        },
+
+    ]
+
+
+    //Cafes Popups
+    for (let coffee of COFFEE) {
+        L.marker([coffee.lat, coffee.lng], {
+            icon: L.icon({
+                iconUrl: `icons/coffee.png`,
+                popupAnchor: [0, -37],
+                iconAnchor: [16, 37],
+            })
+        })
+            .addTo(themaLayer.coffee)
+            .bindPopup(`<b>${coffee.title}</b> <br> ${coffee.ort} | <a href="${coffee.link}">Zur Website</a> 
+`)
+    };
 
     //Maßstab 
     L.control.scale({
--- rad/map5.js
+++ rad/map5.js
@@ -1,7 +1,7 @@
 //Map initialisieren
 let lat5 = 47.268333;
 let lng5 = 11.393333;
-let zoom5 = 9;
+let zoom5 = 11;
 
 document.addEventListener('DOMContentLoaded', function () {
     let map5 = L.map("map5", {
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', function () {
         gestureHandling: true,
     }).setView([lat5, lng5], 11);
 
+
+    // thematische Layer
+    let themaLayer = {
+        bikeshop: L.featureGroup(),
+        coffee: L.featureGroup()
+    };
 
     // WMTS Hintergrundlayer 
     let eGrundkarteTirol = {
@@ -28,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Hintergrundlayer 
-    L.control.layers({
+    let layerControl = L.control.layers({
         "eGrundkarte Tirol Sommer": L.layerGroup([
             eGrundkarteTirol.sommer,
             eGrundkarteTirol.nomenklatur
@@ -38,6 +44,9 @@ document.addEventListener('DOMContentLoaded', function () {
             eGrundkarteTirol.nomenklatur
         ]),
         "OpenStreetMap": openStreetMap,
+    }, {
+        "Fahrradgeschäfte/-reparatur": themaLayer.bikeshop.addTo(map5),
+        "Cafés/Restaurants": themaLayer.coffee.addTo(map5),
     }).addTo(map5);
 
     //Style Elevation
@@ -51,6 +60,170 @@ document.addEventListener('DOMContentLoaded', function () {
         edgeScale: false,
     }).addTo(map5);
     controlElevation.load("data/sellrain.gpx");
+
+
+    //Bikeshops Liste
+    const BIKESHOP = [
+        {
+            title: "Alpin Bike - Fahrradservice & Reparatur",
+            link: "http://www.alpin-bike.at/",
+            ort: "Innsbruck",
+            lat: 47.27877864015659,
+            lng: 11.370899941018507,
+
+        },
+        {
+            title: "Bike Klinik Innsbruck",
+            link: "https://www.bike-klinik.com/",
+            ort: "Innsbruck",
+            lat: 47.27482897493371,
+            lng: 11.404881058358859,
+        },
+        {
+            title: "Fahrradladen Innsbruck",
+            link: "https://www.fahrradladen-innsbruck.at/",
+            ort: "Innsbruck",
+            lat: 47.25522225775388,
+            lng: 11.396223510784315,
+        },
+        {
+            title: "Veloflott GmbH",
+            link: "https://www.veloflott.at/",
+            ort: "Innsbruck",
+            lat: 47.276823813800476,
+            lng: 11.412865313018468,
+        },
+        {
+            title: "WAMS Radwerkstatt Conrad",
+            link: "-",
+            ort: "Innsbruck",
+            lat: 47.262814924800566,
+            lng: 11.378780777203447,
+        },
+        {
+            title: "Radsport Neuner GmbH",
+            link: "https://www.bikestoreneuner.com/",
+            ort: "Kematen in Tirol",
+            lat: 47.26420710613028,
+            lng: 11.269072803358116,
+        },
+        {
+            title: "Bikesport Baumgartner",
+            link: "https://www.sport-baumgartner.at/",
+            ort: "Völs",
+            lat: 47.25835240590591,
+            lng: 11.32184908194286,
+        },
+        {
+            title: "Radl Werkstatt - MANG Thomas",
+            link: "-",
+            ort: "Zirl",
+            lat: 47.27074145483338,
+            lng: 11.253307261129905,
+        },
+
+    ]
+
+
+    //Bikeshops Popups
+    for (let bikeshop of BIKESHOP) {
+        L.marker([bikeshop.lat, bikeshop.lng], {
+            icon: L.icon({
+                iconUrl: `icons/bicycle_shop.png`,
+                popupAnchor: [0, -37],
+                iconAnchor: [16, 37],
+            })
+        })
+            .addTo(themaLayer.bikeshop)
+            .bindPopup(`<b>${bikeshop.title}</b> <br> ${bikeshop.ort} | <a href="${bikeshop.link}">Zur Website</a> 
+`)
+    };
+
+    //Cafes Liste
+    const COFFEE = [
+        {
+            title: "manni.coffee",
+            link: "-",
+            ort: "Innsbruck",
+            lat: 47.26537316391705,
+            lng: 11.394063993012983,
+
+        },
+        {
+            title: "haepinest",
+            link: "https://haepinest.at/",
+            ort: "Innsbruck",
+            lat: 47.271126885551034,
+            lng: 11.391991823597085,
+        },
+        {
+            title: "coffeekult Kaffeerösterei",
+            link: "http://www.coffeekult.com/",
+            ort: "Innsbruck",
+            lat: 47.25652829874937,
+            lng: 11.395917733227401,
+        },
+        {
+            title: "Kostbar Mutters",
+            link: "http://www.kostbar.tirol/",
+            ort: "Mutters",
+            lat: 47.22939567582593,
+            lng: 11.385784905930416,
+        },
+        {
+            title: "Kaffeestub'n am Pavillon",
+            link: "https://www.herold.at/gelbe-seiten/axams/kPpTp/kaffeestubn-am-pavillon/",
+            ort: "Axams",
+            lat: 47.231891693884805,
+            lng: 11.27856328952412,
+        },
+        {
+            title: "schauplats / interior café wohnen",
+            link: "http://www.schauplats.at/",
+            ort: "Axams",
+            lat: 47.22862580345009,
+            lng: 11.278181230995395,
+        },
+        {
+            title: "Gästehaus Edelweiss",
+            link: "-",
+            ort: "Tanneben",
+            lat: 47.21305591724297,
+            lng: 11.223648721193618,
+        },
+        {
+            title: "Cafe Anni",
+            link: "-",
+            ort: "Sellrain",
+            lat: 47.21336145378785,
+            lng: 11.21378415332635,
+        },
+        {
+            title: "Cafe Grünfelder",
+            link: "https://pizzeria-cafe-gruenfelder.eatbu.com/",
+            ort: "Oberperfuss",
+            lat: 47.23626521786019,
+            lng: 11.244025770058283,
+        },
+
+
+    ]
+
+
+    //Cafes Popups
+    for (let coffee of COFFEE) {
+        L.marker([coffee.lat, coffee.lng], {
+            icon: L.icon({
+                iconUrl: `icons/coffee.png`,
+                popupAnchor: [0, -37],
+                iconAnchor: [16, 37],
+            })
+        })
+            .addTo(themaLayer.coffee)
+            .bindPopup(`<b>${coffee.title}</b> <br> ${coffee.ort} | <a href="${coffee.link}">Zur Website</a> 
+`)
+    };
+
 
     //Maßstab 
     L.control.scale({
